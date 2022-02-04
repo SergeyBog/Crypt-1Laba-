c6a4c647a18f4e2e6e8fde330635c3853c71f66b
SergeyBog <serg123bb@gmail.com>
2022-02-04 09:50:26 +0200

    Add project files.

---
+++ Crypt(1Laba)/Part_0/GetText.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypt_1Laba_.Part_0
+{
+    public class GetText
+    {
+        public string GetCorrectString()
+        {
+            var textList = File.ReadAllText(@"C:\Users\Sergey\source\repos\Crypt(1Laba)\Crypt(1Laba)\Part_0\Task0.txt");
+            int numverOfChars = textList.Length;
+            string result = "";
+            List<string> binaryCode = new List<string>();
+            for (int i = 0; i < textList.Length; i++)
+            {
+                result += textList[i];
+                if (result.Length % 8 == 0)
+                {
+                    binaryCode.Add(result);
+                    result = "";
+                }
+            }
+            result = "";
+            int[] rofl = new int[binaryCode.Count];
+            for (int j = 0; j < binaryCode.Count; j++)
+            {
+                rofl[j] = Convert.ToInt32(binaryCode[j], 2);
+
+            }
+            for (int k = 0; k < rofl.Length; k++)
+            {
+                result += Convert.ToChar(rofl[k]);
+            }
+            byte[] data = Convert.FromBase64String(result);
+            string decodedString = Encoding.UTF8.GetString(data);
+            return decodedString;
+        }
+    }
+}
---
+++ Crypt(1Laba)/Program.cs
@@ -0,0 +1,24 @@
+﻿using Crypt_1Laba_.Part_0;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypt_1Laba_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstPart());
+            Console.ReadKey();
+        }
+        public static string FirstPart()
+        {
+            var worker = new GetText();
+            string result = worker.GetCorrectString();
+            return result;
+        }
+    }
+}
